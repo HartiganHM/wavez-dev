5575e5bc678f958a15f0f643f00154224bc85cc9
Hugh Hartigan <hartigan.hm@gmail.com>
2023-06-10 09:52:33 -0600

    chore: Import all types to schema

--- graphql/schema.ts
+++ graphql/schema.ts
@@ -1,4 +1,10 @@
 // graphql/schema.ts
+import './types/AccessKey';
+import './types/Device';
+import './types/NanoleafAuthToken';
+import './types/NanoleafProperties';
+import './types/Palette';
+import './types/User';
 
 import { builder } from './builder';
 
