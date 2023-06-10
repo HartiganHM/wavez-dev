2e55cb6455b41db8871271abb07d3af0f9e1fc4a
Hugh Hartigan <hartigan.hm@gmail.com>
2023-06-10 09:37:53 -0600

    chore: Update linting for lib/prisma

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -1,5 +1,5 @@
 // /lib/prisma.ts
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
 
@@ -8,11 +8,11 @@ declare global {
 }
 
 if (process.env.NODE_ENV === 'production') {
-  prisma = new PrismaClient()
+  prisma = new PrismaClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient()
+    global.prisma = new PrismaClient();
   }
-  prisma = global.prisma
+  prisma = global.prisma;
 }
-export default prisma
+export default prisma;
