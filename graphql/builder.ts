46a01b2e78418ce4d6835476949a23e01b414e6a
Hugh Hartigan <hartigan.hm@gmail.com>
2023-06-10 10:42:47 -0600

    chore: Add WifiDevice/DeviceType to builder scalar

--- graphql/builder.ts
+++ graphql/builder.ts
@@ -1,12 +1,12 @@
-// graphql/builder.ts
-
 // 1. Defines all the libraries and utilities that will be needed
 import SchemaBuilder from '@pothos/core';
 import PrismaPlugin from '@pothos/plugin-prisma';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
 import { DateResolver } from 'graphql-scalars';
+import { IDevice } from 'local-devices';
 
 import prisma from '../lib/prisma';
+import { types as deviceTypes } from './device/definitions';
 
 // 2. Creates a new SchemaBuilder instance
 export const builder = new SchemaBuilder<{
@@ -17,7 +17,15 @@ export const builder = new SchemaBuilder<{
       Input: Date;
       Output: Date;
     };
-  }
+    WifiDevice: {
+      Input: IDevice;
+      Output: IDevice;
+    };
+    DeviceType: {
+      Input: deviceTypes.DeviceType;
+      Output: deviceTypes.DeviceType;
+    };
+  };
 }>({
   // 4.Defines options for the SchemaBuilder such as the plugins and
   // the Prisma Client instance that will be used
@@ -29,6 +37,12 @@ export const builder = new SchemaBuilder<{
 
 // 5. Add custom scalar types
 builder.addScalarType('DateTime', DateResolver, {});
+builder.scalarType('DeviceType', {
+  serialize: (n) => n,
+});
+builder.scalarType('WifiDevice', {
+  serialize: (n) => n,
+});
 
 // 6. Creates a queryType with a query called ok that returns a boolean
 builder.queryType({
