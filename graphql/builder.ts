3b26b89b052ad30f7677b1816369e585ebffe9e7
Hugh Hartigan <hartigan.hm@gmail.com>
2023-06-10 09:35:02 -0600

    feat: Update builder with pothos add DateTime scalar type

--- graphql/builder.ts
+++ graphql/builder.ts
@@ -1,21 +1,36 @@
-import SchemaBuilder from "@pothos/core";
+// graphql/builder.ts
+
+// 1. Defines all the libraries and utilities that will be needed
+import SchemaBuilder from '@pothos/core';
 import PrismaPlugin from '@pothos/plugin-prisma';
 import type PrismaTypes from '@pothos/plugin-prisma/generated';
-import prisma from "../lib/prisma";
-import RelayPlugin from "@pothos/plugin-relay";
-import {createContext} from './context'
+import { DateResolver } from 'graphql-scalars';
 
+import prisma from '../lib/prisma';
+
+// 2. Creates a new SchemaBuilder instance
 export const builder = new SchemaBuilder<{
-  PrismaTypes: PrismaTypes,
-  Context: ReturnType<typeof createContext>,
+  // 3. Defines the static types that will be used in creating the GraphQL schema
+  PrismaTypes: PrismaTypes;
+  Scalars: {
+    DateTime: {
+      Input: Date;
+      Output: Date;
+    };
+  }
 }>({
-  plugins: [PrismaPlugin, RelayPlugin],
-  relayOptions: {},
+  // 4.Defines options for the SchemaBuilder such as the plugins and
+  // the Prisma Client instance that will be used
+  plugins: [PrismaPlugin],
   prisma: {
     client: prisma,
-  }
-})
+  },
+});
 
+// 5. Add custom scalar types
+builder.addScalarType('DateTime', DateResolver, {});
+
+// 6. Creates a queryType with a query called ok that returns a boolean
 builder.queryType({
   fields: (t) => ({
     ok: t.boolean({
@@ -23,5 +38,3 @@ builder.queryType({
     }),
   }),
 });
-
-builder.mutationType({})
