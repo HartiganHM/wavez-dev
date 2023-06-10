d5b39d6374c02a3aafcbb0149e9e941e0efc1b67
Hugh Hartigan <hartigan.hm@gmail.com>
2023-06-10 09:31:02 -0600

    build: User GQL builder type

--- graphql/types/User.ts
+++ graphql/types/User.ts
@@ -1,38 +1,23 @@
 // /graphql/types/User.ts
-import { builder } from "../builder";
+import { builder } from '../builder';
 
 builder.prismaObject('User', {
   fields: (t) => ({
     id: t.exposeID('id'),
-    email: t.exposeString('email', { nullable: true, }),
-    image: t.exposeString('image', { nullable: true, }),
-    role: t.expose('role', { type: Role, }),
-    bookmarks: t.relation('bookmarks'),
-  })
-})
+    email: t.exposeString('email'),
+    name: t.exposeString('name'),
+    devices: t.relation('devices'),
+    palettes: t.relation('palettes'),
+    role: t.expose('role', { type: Role }),
+    invites: t.exposeInt('invites'),
+    invitedById: t.exposeID('invitedById', { nullable: true }),
+    invitedBy: t.relation('invitedBy'),
+    invitedUserToUser: t.relation('invitedUserToUser'),
+    createdAt: t.expose('createdAt', { type: 'DateTime' }),
+    updatedAt: t.expose('updatedAt', { type: 'DateTime' }),
+  }),
+});
 
 const Role = builder.enumType('Role', {
-  values: ['USER', 'ADMIN'] as const,
-})
-
-builder.queryField('favorites', (t) =>
-  t.prismaField({
-    type: 'User',
-    resolve: async (query, _parent, _args, ctx) => {
-      if (!(await ctx).user) {
-        throw new Error("You have to be logged in to perform this action")
-      }
-
-      const user = await prisma.user.findUnique({
-        ...query,
-        where: {
-          email: (await ctx).user?.email,
-        }
-      })
-
-      if (!user) throw Error('User does not exist');
-
-      return user
-    }
-  })
-)
+  values: ['ADMIN', 'SUPPORTER', 'ALPHA', 'BETA', 'BASIC'] as const,
+});
