0c8a653dc8dbf616a4d542ee5c4a4c8eab7f7b87
Hugh Hartigan <hartigan.hm@gmail.com>
2023-06-11 12:44:07 -0600

    feat: Add Nav to Providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -5,11 +5,15 @@ import { ApolloProvider } from '@apollo/client';
 import { NextUIProvider } from '@nextui-org/react';
 
 import apolloClient from 'lib/apollo';
+import Nav from 'src/components/nav';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <NextUIProvider>
-      <ApolloProvider client={apolloClient}>{children}</ApolloProvider>
+      <ApolloProvider client={apolloClient}>
+        <Nav />
+        {children}
+      </ApolloProvider>
     </NextUIProvider>
   );
 }
