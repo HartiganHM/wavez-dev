b6400bd98fa33851fd96bdc9c02a4e4dc9bf12d7
Hugh Hartigan <hartigan.hm@gmail.com>
2023-06-11 11:56:56 -0600

    fix: Add /src to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,7 @@ const { typewindTransforms } = require('typewind/transform');
 module.exports = {
   content: {
     files: [
+      './src/**/*.{js,ts,jsx,tsx}',
       './app/**/*.{js,ts,jsx,tsx}',
       './pages/**/*.{js,ts,jsx,tsx}',
       './components/**/*.{js,ts,jsx,tsx}',
