388789cf50acc707d0e141769e089b9525d999f8
Hugh Hartigan <hartigan.hm@gmail.com>
2023-06-11 00:43:37 -0600

    feat: Render WifiDevices on landing page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,103 +1,45 @@
 // /pages/index.tsx
-import Head from "next/head";
-import { gql, useQuery, useMutation } from "@apollo/client";
-import { AwesomeLink } from "../components/AwesomeLink";
-import type { Link as Node } from "@prisma/client";
-import Link from "next/link";
-import { useUser } from "@auth0/nextjs-auth0/client";
+import { gql, useQuery } from '@apollo/client';
+import { Container, Card, Grid, Text } from '@nextui-org/react';
+import { IDevice } from 'local-devices';
 
-const AllLinksQuery = gql`
-  query allLinksQuery($first: Int, $after: ID) {
-    links(first: $first, after: $after) {
-      pageInfo {
-        endCursor
-        hasNextPage
-      }
-      edges {
-        cursor
-        node {
-          imageUrl
-          url
-          title
-          category
-          description
-          id
-        }
-      }
+const AllWifiDevices = gql`
+  query {
+    wifiDevices {
+      ip
+      name
+      mac
     }
   }
 `;
 
-function Home() {
-  const { user } = useUser()
-  const { data, loading, error, fetchMore } = useQuery(AllLinksQuery, {
-    variables: { first: 3 },
-  });
-
-  if (!user) {
-    return (
-      <div className="flex items-center justify-center">
-        To view the awesome links you need to{' '}
-        <Link href="/api/auth/login" className=" block bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
-          Login
-        </Link>
-      </div>
-    );
-  }
+export default function Home() {
+  const { data, loading, error } = useQuery(AllWifiDevices);
+  console.log(data);
 
   if (loading) return <p>Loading...</p>;
   if (error) return <p>Oh no... {error.message}</p>;
 
-  const { endCursor, hasNextPage } = data?.links.pageInfo;
-
   return (
-    <div>
-      <Head>
-        <title>Awesome Links</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <div className="container mx-auto max-w-5xl my-20">
-        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-          {data?.links.edges.map(({ node }: { node: Node }) => (
-            <Link href={`/link/${node.id}`}>
-              <AwesomeLink
-                key={node.id}
-                title={node.title}
-                category={node.category}
-                url={node.url}
-                id={node.id}
-                description={node.description}
-                imageUrl={node.imageUrl}
-              />
-            </Link>
-          ))}
-        </div>
-        {hasNextPage ? (
-          <button
-            className="px-4 py-2 bg-blue-500 text-white rounded my-10"
-            onClick={() => {
-              fetchMore({
-                variables: { after: endCursor },
-                updateQuery: (prevResult, { fetchMoreResult }) => {
-                  fetchMoreResult.links.edges = [
-                    ...prevResult.links.edges,
-                    ...fetchMoreResult.links.edges,
-                  ];
-                  return fetchMoreResult;
-                },
-              });
-            }}
-          >
-            more
-          </button>
-        ) : (
-          <p className="my-10 text-center font-medium">
-            You've reached the end!{" "}
-          </p>
-        )}
-      </div>
-    </div>
+    <Container>
+      <Grid.Container gap={2}>
+        {data.wifiDevices.map((wifiDevice: IDevice) => (
+          <Grid xs={6} key={wifiDevice.ip}>
+            <Card css={{ mw: '400px' }} variant="bordered">
+              <Card.Header>
+                <Text b>{wifiDevice.name}</Text>
+              </Card.Header>
+
+              <Card.Divider />
+
+              <Card.Body>
+                <Text>{wifiDevice.ip}</Text>
+                <Text>{wifiDevice.mac}</Text>
+              </Card.Body>
+            </Card>
+          </Grid>
+        ))}
+      </Grid.Container>
+    </Container>
   );
 }
-
-export default Home;
