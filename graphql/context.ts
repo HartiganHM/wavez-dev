7a885b6b966d6742a13ee247acd0cfb78253f03b
Hugh Hartigan <hhartigan@twilio.com>
2023-06-15 20:14:59 -0600

    feat: Add Prisma user to context

--- graphql/context.ts
+++ graphql/context.ts
@@ -1,5 +1,6 @@
 // graphql/context.ts
 import { getSession } from '@auth0/nextjs-auth0';
+import prisma from 'lib/prisma';
 import type { NextApiRequest, NextApiResponse } from 'next';
 
 export async function createContext({
@@ -10,15 +11,24 @@ export async function createContext({
   res: NextApiResponse;
 }) {
   const session = await getSession(req, res);
+  console.log(session);
 
   // if the user is not logged in, return an empty object
   if (!session || typeof session === 'undefined') return {};
 
-  const { user, accessToken } = session;
+  const { user: authUser, accessToken } = session;
+
+  const user = await prisma.user.findUnique({
+    where: {
+      email: authUser.email,
+    },
+  });
+
   console.log(user);
 
   return {
     user,
+    authUser,
     accessToken,
   };
 }
