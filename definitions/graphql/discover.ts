24ddf582ca656da4c5966fd617baa339e06288ea
Hugh Hartigan <hhartigan@twilio.com>
2023-06-17 10:38:21 -0600

    build: Add ADD_NEW_NANOLEAF_DEVICE mutation to client

--- definitions/graphql/discover.ts
+++ definitions/graphql/discover.ts
@@ -1,6 +1,6 @@
 import { gql } from '@apollo/client';
 
-export const GET_WIFIF_DEVICES_BY_TYPE = gql`
+export const GET_WIFI_DEVICES_BY_TYPE = gql`
   query WifiDevicesByType($type: WifiDeviceType!) {
     wifiDevicesByType(type: $type) {
       ip
@@ -9,3 +9,11 @@ export const GET_WIFIF_DEVICES_BY_TYPE = gql`
     }
   }
 `;
+
+export const ADD_NEW_NANOLEAF_DEVICE = gql`
+  mutation AddNewNanoleafDevice($input: AuthenticateWithDeviceInput!) {
+    authenticateWithDeviceByUserId(input: $input) {
+      name
+    }
+  }
+`
