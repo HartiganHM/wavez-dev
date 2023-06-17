b33a0f49f59cd4713e1b118df0c1d2e7f8327801
Hugh Hartigan <hhartigan@twilio.com>
2023-06-17 11:06:55 -0600

    feat: Add ToastContainer to Providers

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,18 +1,20 @@
-// app/providers.tsx
 'use client';
 
 import { UserProvider } from '@auth0/nextjs-auth0/client';
 import { ApolloProvider } from '@apollo/client';
 import { NextUIProvider } from '@nextui-org/react';
+import { ToastContainer } from 'react-toastify';
 
 import apolloClient from 'lib/apollo';
 import Nav from 'src/components/nav';
+import 'react-toastify/dist/ReactToastify.css';
 
 export function Providers({ children }: { children: React.ReactNode }) {
   return (
     <UserProvider>
       <ApolloProvider client={apolloClient}>
         <NextUIProvider>
+          <ToastContainer position="top-right" />
           <Nav />
           {children}
         </NextUIProvider>
